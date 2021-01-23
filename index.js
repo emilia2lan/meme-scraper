a26190ba36cdbac77703dafed486f27273b2efde
Emilia Tulan <emilia.tulan@gmail.com>
2021-01-23 17:50:04 +0100

    Add first task downloading 10 figures

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// brings back plain text or html
+const axios = require('axios');
+const { JSDOM } = require('jsdom');
+const fs = require('fs');
+const request = require('request');
 
-const fetch = require('node-fetch');
+axios
+  .get('https://memegen-link-examples-upleveled.netlify.app/')
+  .then(async (res) => {
+    // res.data holds the <html>
 
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then(function (res) {
-    return res.text();
-  })
-  .then(function (body) {
-    console.log(body);
+    const dom = new JSDOM(res.data);
+
+    // filters all imagines and get array
+    const images = dom.window.document.querySelectorAll('img');
+
+    for (let i = 0; i < 10; i++) {
+      const fileSource = images[i].getAttribute('src');
+      console.log(fileSource);
+
+      // brings the images in .jpg and add names
+      request(`${fileSource}`).pipe(
+        fs.createWriteStream(`${__dirname}/images/img_${i + 1}.jpg`),
+      );
+    }
   });
